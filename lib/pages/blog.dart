0c3c216b22ccb1d496affd426a5cb760ad6e8ff3
ayeshaamjad01 <ashamjad007@gmail.com>
2024-11-18 13:06:10 +0500

    18-11

---
+++ lib/pages/blog.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Blog extends StatelessWidget {
+  const Blog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Blog")),
+    );
+  }
+}
